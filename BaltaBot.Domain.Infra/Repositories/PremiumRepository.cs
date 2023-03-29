ca7bfd639697d92e827709e2ee12fd04fd5b28fd
nmapv <hablarconandre@gmail.com>
2023-03-29 09:30:11 -0300

    chore: removal of unnecessary code

--- BaltaBot.Domain.Infra/Repositories/PremiumRepository.cs
+++ BaltaBot.Domain.Infra/Repositories/PremiumRepository.cs
@@ -14,20 +14,11 @@ namespace BaltaBot.Domain.Infra.Repositories
         public async Task<IEnumerable<Premium>> GetInactives()
         {
             var today = DateTime.Now;
-            var premiumDictionary = new Dictionary<Guid, Premium>();
             var result = await DataContext.connection.QueryAsync<Premium, Person, Premium>(@"select * from Premium pr, Person pe where pr.PersonId = pe.Id and pr.ClosedAt<=@today",
                 (premium, person) =>
                 {
-                    Premium premiumEntry;
-
-                    if (!premiumDictionary.TryGetValue(premium.Id, out premiumEntry))
-                    {
-                        premiumEntry = premium;
-                        premiumDictionary.Add(premiumEntry.Id, premiumEntry);
-                    }
-
-                    premiumEntry.SetPerson(person);
-                    return premiumEntry;
+                    premium.SetPerson(person);
+                    return premium;
                 },
                 splitOn: "Id",
                 param: new { today }
